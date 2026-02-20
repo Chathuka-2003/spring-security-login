23d0ec97366f256b2d45b2248989d78cc57153c8
Chathuka Priyankara Edirisinghe <chathukapriyankaraedirisinghe@gmail.com>
2026-02-20 13:58:26 +0530

    Add basic security with jwt

---
+++ src/main/java/com/codewithck/jwt/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.codewithck.jwt.controller;
+
+import com.codewithck.jwt.service.JWTService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    private final JWTService jwtService;
+
+    public HomeController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @GetMapping
+    public String getHello(){
+        return "Hi ....";
+    }
+    @PostMapping("/login")
+    public String login(){
+        return jwtService.getJWTToken();
+    }
+    @GetMapping("username")
+    public String getUserName(@RequestParam String token){
+        return jwtService.getUserName(token);
+    }
+}
---
+++ src/main/java/com/codewithck/jwt/service/JWTService.java
@@ -0,0 +1,42 @@
+package com.codewithck.jwt.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JWTService {
+
+    private final SecretKey secretKey;
+
+    public JWTService(){
+        try{
+            SecretKey k = KeyGenerator.getInstance("HmacSHA256").generateKey();
+            secretKey = Keys.hmacShaKeyFor(k.getEncoded());
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    public String getJWTToken(){
+        return Jwts.builder()
+                .subject("CKCoding")
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+1000*60*15))
+                .signWith(secretKey)
+                .compact();
+    }
+    //check token validation and get data from the token
+    public String getUserName(String token){
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
